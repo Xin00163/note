afc318fe9a1082f4392e40bfb8b8bd7c6fdc3367
Xin Wang <wangxin00163@yahoo.co.uk>
2018-01-09 23:40:55 +0000

    Add a listMaker function to note-controller

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,18 +1,19 @@
 (function(exports) {
-
-
-  var Controller = function(list = new List){
+  function Controller(list = new List()){
     this.list = list
     note = new Note("Favourite drink: seltzer")
     this.list.addNoteToList(note)
-    view = new View(list)
-
-    document.getElementById("app").innerHTML = view.returnHTML()
+    this.view = new View(list)
   };
-
   exports.Controller = Controller;
 })(this);
 
+Controller.prototype.listMaker = function(element){
+  var element = document.getElementById("app");
+  element.innerHTML = this.view.returnHTML();
+};
+
+
 window.onload = function() {
-  controller = new Controller();
+
 };
