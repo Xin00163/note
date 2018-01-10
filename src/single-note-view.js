5e50739d63f7e75641f3b9231023e7e693bd59a7
Xin Wang <wangxin00163@yahoo.co.uk>
2018-01-10 15:51:04 +0000

    pass test for single note view

---
+++ src/single-note-view.js
@@ -0,0 +1,12 @@
+(function(exports) {
+  function singleNoteView(note){
+    this.newnote = note;
+  };
+
+  singleNoteView.prototype.printNote = function(){
+    return "<li>" + this.newnote.text + "</li>";
+
+  };
+
+  exports.singleNoteView = singleNoteView;
+})(this);
